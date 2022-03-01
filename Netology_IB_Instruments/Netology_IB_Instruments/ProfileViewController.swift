ac030b4b44ff060ab4b93c8b3c097d5620e21f0b
Alena Sabadash <alena_sbsh@mail.ru>
2022-03-01 20:44:49 +0500

    Добавлено домашнее задание №1

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Alena Sabadash on 27.02.2022.
+//  Copyright © 2022 Alena Sabadash. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Alena Sabadash on 27.02.2022.
+//  Copyright © 2022 Alena Sabadash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

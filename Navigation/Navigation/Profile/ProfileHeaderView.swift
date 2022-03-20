41b9f6221177e94e7047331ea58bf9cbc9e8ec26
Alena Sabadash <alena_sbsh@mail.ru>
2022-03-20 19:53:07 +0500

    Добавлено домашнее задание №4

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -8,13 +8,81 @@
 import UIKit
 
 class ProfileHeaderView: UIView {
+    
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "doggie")
+        imageView.frame.size.width = 150
+        imageView.frame.size.height = 150
+        imageView.layer.borderWidth = 3
+        imageView.layer.masksToBounds = false
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.clipsToBounds = true
+        return imageView
+    }()
 
-    @IBOutlet var contentView: UIView!
-    @IBOutlet weak var avatar: UIImageView!
-    @IBOutlet weak var username: UILabel!
-    @IBOutlet weak var status: UILabel!
-    @IBOutlet weak var showStatusButton: UIButton!
-
+    private lazy var fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hipster Cat"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 18.0)
+        return label
+    }()
+    
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 14.0)
+        label.text = "Doing something..."
+        return label
+    }()
+    
+    private lazy var statusTextField: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemBlue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var setStatusButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemPink
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Show status", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor.systemBlue
+        button.titleLabel?.textColor = .white
+        button.layer.cornerRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.shadowRadius = 4
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var labelsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .black
+        return stackView
+    }()
+    
+    private lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -26,44 +94,39 @@ class ProfileHeaderView: UIView {
     }
 
     private func setupView() {
-        Bundle.main.loadNibNamed("ProfileHeaderView", owner: self, options: nil)
-        addSubview(contentView)
-        addSubview(avatar)
-
-        contentView.backgroundColor = .lightGray
-
-        avatar.image = UIImage(named: "doggie")
-        avatar.frame.size.width = 150
-        avatar.frame.size.height = 150
-        avatar.layer.borderWidth = 3
-        avatar.layer.masksToBounds = false
-        avatar.layer.borderColor = UIColor.white.cgColor
-        avatar.layer.cornerRadius = avatar.frame.size.width / 2
-        avatar.clipsToBounds = true
-
-        username.text = "Hipster Cat"
-        username.textColor = .black
-        username.font = UIFont.boldSystemFont(ofSize: 18.0)
-
-        status.text = "Doing something..."
-        status.textColor = .gray
-        status.font = UIFont.systemFont(ofSize: 14.0)
-
-        showStatusButton.setTitle("Show status", for: .normal)
-        showStatusButton.setTitleColor(.white, for: .normal)
-        showStatusButton.backgroundColor = UIColor.systemBlue
-        showStatusButton.titleLabel?.textColor = .white
-        showStatusButton.layer.cornerRadius = 4
-        showStatusButton.layer.shadowColor = UIColor.black.cgColor
-        showStatusButton.layer.shadowOpacity = 0.7
-        showStatusButton.layer.shadowRadius = 4
-        showStatusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-        showStatusButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        showStatusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        self.addSubview(infoStackView)
+        self.addSubview(setStatusButton)
+        self.infoStackView.addSubview(avatarImageView)
+        self.infoStackView.addSubview(labelsStackView)
+        self.labelsStackView.addSubview(fullNameLabel)
+        self.labelsStackView.addSubview(statusLabel)
+        
+        let imageViewAspectRatio = self.avatarImageView.heightAnchor.constraint(equalTo: self.avatarImageView.widthAnchor, multiplier: 1.0)
+        
+        let topConstraint = self.infoStackView.topAnchor.constraint(equalTo: self.topAnchor)
+        let leadingConstraint = self.infoStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20)
+        let trailingConstraint = self.infoStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+        
+        let buttonTopConstraint = self.setStatusButton.topAnchor.constraint(equalTo: self.infoStackView.bottomAnchor, constant: 20)
+        
+        let leadingButtonConstraint = self.setStatusButton.leadingAnchor.constraint(equalTo: self.infoStackView.leadingAnchor)
+        let trailingButtonConstraint = self.setStatusButton.trailingAnchor.constraint(equalTo: self.infoStackView.trailingAnchor)
+        let bottomButtonConstraint = self.setStatusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        
+        NSLayoutConstraint.activate([
+            imageViewAspectRatio,
+            topConstraint,
+            leadingConstraint,
+            trailingConstraint,
+            buttonTopConstraint,
+            leadingButtonConstraint,
+            trailingButtonConstraint,
+            bottomButtonConstraint
+        ])
     }
 
     @objc func buttonPressed() {
-        let status = status.text ?? ""
+        let status = statusLabel.text ?? ""
         print("Status: \(status)")
     }
 
